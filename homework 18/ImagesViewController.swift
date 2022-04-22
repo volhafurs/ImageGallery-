afb6f44bae951c19c42e80b81728f4d510ba1815
volhafurs <volhafurss@gmail.com>
2022-04-22 15:37:26 +0200

    add homework 18

---
+++ homework 18/ImagesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ImagesViewController.swift
+//  homework 18
+//
+//  Created by Volha Furs on 22.04.22.
+//
+
+import UIKit
+
+class ImagesViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    var imagesArray = [UIImage]()
+    var imageURLArray = [URL]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func onAddNewImagesButton(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func onSeeButton(_ sender: Any) {
+        print(imagesArray)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage]
+        let imageURL = info[.referenceURL]
+        
+        if imageURLArray.contains(imageURL as! URL){
+            print("already added")
+        } else {
+            imageURLArray.append(info[.referenceURL] as! URL)
+            imagesArray.append(image as! UIImage)
+            ImageView.image = image as! UIImage
+            ImageView.contentMode = .scaleAspectFill
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
